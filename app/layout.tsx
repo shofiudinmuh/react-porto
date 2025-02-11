ea277b0cac85524b339349dd5488f5d09bd47d35
shofiudinmuh <shofiudinmuh@gmail.com>
2025-02-11 17:55:18 +0700

    component : add hero componen with motion clsx tailwind, install mini-svg, install tailwind-animate, install next-theme and react-icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,36 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
+import { ThemeProvider } from './providers';
 
 const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
+    variable: '--font-geist-sans',
+    subsets: ['latin'],
+    display: 'swap', // Tambahkan ini
 });
 
 const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
+    variable: '--font-geist-mono',
+    subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
-  title: 'Shofiudin Portofolio',
-  description: 'Modern & Minimalist JS Protofolio',
+    title: 'Shofiudin Portofolio',
+    description: 'Modern & Minimalist JS Protofolio',
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
-    </html>
-  );
+    return (
+        <html lang='en'>
+            <body suppressHydrationWarning className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+                <ThemeProvider attribute='class' defaultTheme='dark' enableSystem disableTransitionOnChange>
+                    {children}
+                </ThemeProvider>
+            </body>
+        </html>
+    );
 }
